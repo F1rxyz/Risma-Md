a7c4db2465c3586aad6e4afb80ecd62188de08c8
F1rxyz <110138791+F1rxyz@users.noreply.github.com>
2022-08-08 09:03:17 +0700

    Update config.js

--- config.js
+++ config.js
@@ -37,11 +37,11 @@ global.eror = '_*𝒎𝒂𝒂𝒇 𝒔𝒆𝒓𝒗𝒆𝒓 𝒆𝒓𝒓𝒐𝒓.
 
 //========Url Template Buttons=========//
 global.dtu = '𝑌𝑜𝑢𝑡𝑢𝑏𝑒'
-global.urlnya = "https://youtube.com/channel/UCW7iXlE7TgvJMIXQck4NYBQ"
+global.urlnya = "https://youtube.com/channel/Firxyz"
 
 //============= callButtons =============//
 global.dtc = '𝒄𝒂𝒍𝒍 𝒐𝒘𝒏𝒆𝒓'
-global.phn = '62 857-1313-7903'
+global.phn = '6285806459088'
 
 //============= Games ================//
 global.benar = '_*Benar*_'
